66d852a9f4d8d6d1283affc0b65c22f88706c91d
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-07-09 00:04:03 -0500

    Update 2-8.1.1模拟哈希表-840. 模拟散列表-开放寻址法.cpp
    https://www.acwing.com/activity/content/problem/content/890/
    
    https://www.acwing.com/problem/content/842/

--- 算法基础课/W2数据结构/2-8.1.1模拟哈希表-840. 模拟散列表-开放寻址法.cpp
+++ 算法基础课/W2数据结构/2-8.1.1模拟哈希表-840. 模拟散列表-开放寻址法.cpp
@@ -1,25 +1,27 @@
 #include<iostream>
 #include<cstring>
 using namespace std;
-const int N = 2e5 + 3, null = 0x3f3f3f3f;
-int h[N], n;
-int find(int x){
-    int t = (x % N + N ) % N;
-    while(h[t] != null && h[t] != x){
-        ++t;
-        if(t == N) t = 0;
-    }
-    return t;
-}int main(){
-    memset(h, null, sizeof h);
+const int N = 1e5 + 10;
+int h[N], e[N], ne[N], idx, n;
+void insert(int x){
+    int k = (x % N + N ) % N;
+    e[idx] = x, ne[idx] = h[k], h[k] = idx ++ ;
+}
+bool find(int x){
+    int k = (x % N + N ) % N;
+    for(int i = h[k]; i != -1 ; i = ne[i])
+        if (e[i] == x) return true;
+    return false;
+}
+int main(){
     scanf("%d", &n);
+    memset(h, -1, sizeof h);
     while(n--){
-        char op[2];int x;
-        scanf("%s%d", op, &x);
-        if(*op == 'I') h[find(x)] = x;
+        int x; char op[2]; scanf("%s%d", op, &x);
+        if(*op == 'I') insert(x);
         if(*op == 'Q'){
-            if(h[find(x)] == null) puts("No");
-            else puts("Yes");
+            if(find(x)) puts("Yes");
+            else puts("No");
         }
     }
     return 0;
