1e5592a2568c951af033fa0dec8b0fba090b4c83
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-07-09 00:05:23 -0500

    Update 2-8.1.2模拟哈希表-840. 模拟散列表-拉链法.cpp
    https://www.acwing.com/activity/content/problem/content/890/
    
    https://www.acwing.com/problem/content/842/

--- 算法基础课/W2数据结构/2-8.1.2模拟哈希表-840. 模拟散列表-拉链法.cpp
+++ 算法基础课/W2数据结构/2-8.1.2模拟哈希表-840. 模拟散列表-拉链法.cpp
@@ -1,28 +1,25 @@
 #include<iostream>
 #include<cstring>
 using namespace std;
-const int N = 1e5 + 3;
-int h[N], e[N], ne[N], idx, n;
-void insert(int x){
-    int k = (x % N + N) % N;
-    e[idx] = x, ne[idx] = h[k], h[k] = idx++;
-}
-bool find(int x){
+const int N = 2e5 + 3, null = 0x3f3f3f3f;
+int h[N], n;
+int find(int x){
     int k = (x % N + N ) % N;
-    for(int i = h[k]; i != -1 ; i = ne[i])
-        if(e[i] == x) return true;
-    return false;
+    while(h[k] != null && h[k] != x){
+        ++k;
+        if(k == N) k = 0;
+    }
+    return k;
 }
 int main(){
+    memset(h, null, sizeof h);
     scanf("%d", &n);
-    memset(h, -1, sizeof(h));
     while(n--){
-        char op[2]; int x;
-        scanf("%s%d", op, &x);
-        if(*op == 'I') insert(x);
-        if(*op == 'Q') {
-            if(find(x)) puts("Yes");
-            else puts("No");
+        char op[2]; int x; scanf("%s%d", op, &x);
+        if(*op == 'I') h[find(x)] = x;
+        if(*op == 'Q'){
+            if(h[find(x)] == null) puts("No");
+            else puts("Yes");
         }
     }
     return 0;
