b58a2f41d15617f6b633658370f0de76c6b4bf98
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-07-10 23:24:28 -0500

    Update 3-1.2.0 N皇后问题-843. n-皇后问题.cpp

--- 算法基础课/W3搜索与图论/3-1.2.0 N皇后问题-843. n-皇后问题.cpp
+++ 算法基础课/W3搜索与图论/3-1.2.0 N皇后问题-843. n-皇后问题.cpp
@@ -28,3 +28,37 @@ int main(){
     dfs(0);
     return 0;
 }
+
+
+
+//EDITION 1   Time Complexity = O(2^(n^2))
+#include<iostream>
+using namespace std;
+const int N = 1e1;
+char q[N][N];
+bool diagonal[N*2], back_diagonal[N*2], Column[N], Row[N];
+int n;
+void dfs(int row, int col, int nQ){
+    if(col == n) col = 0, ++row;
+    if(row == n){
+        if(nQ == n){
+            for(int i = 0; i < n; ++i) puts(q[i]);
+            puts("");
+        }
+        return;
+    }
+    q[row][col] = '.';
+    dfs(row, col + 1, nQ);
+    if(!(Row[row]||Column[col]||diagonal[row+col]||back_diagonal[n-col+row])){
+        q[row][col] = 'Q';
+        Row[row] = Column[col] = diagonal[row+col] = back_diagonal[n-col+row] = 1;
+        dfs(row,col+1,nQ+1);
+         Row[row] = Column[col] = diagonal[row+col] = back_diagonal[n-col+row] = 0;
+        q[row][col] = '.';
+    }
+}
+int main(){
+    scanf("%d", &n);
+    dfs(0, 0, 0);
+    return 0;
+}
