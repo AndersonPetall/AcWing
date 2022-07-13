ba8962f8ef18038b1c766a4819b78eb0a52c9e23
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-07-13 08:55:22 -0500

    Update 2-6.1.0合并集合-836. 合并集合.cpp

--- 算法基础课/W2数据结构/2-6.1.0合并集合-836. 合并集合.cpp
+++ 算法基础课/W2数据结构/2-6.1.0合并集合-836. 合并集合.cpp
@@ -2,19 +2,21 @@
 using namespace std;
 const int N = 1e5 + 10;
 int p[N];
-int find(int x){
-    if(p[x] != x) p[x] = find(p[x]);  //寻找树根 
-    return p[x];
+int find(int idx)
+{
+    if(p[idx] != idx) p[idx] = find(p[idx]);
+    return p[idx];
 }
 int main(){
     int n, m; scanf("%d%d", &n, &m);
-    for(int i = 0; i <= n; ++i) p[i] = i;  // 初始化
-    while(m--){
-        char op[2];
-        int a, b;
+    for(int i = 0; i < n; ++i) p[i] = i;
+    while(m--)
+    {
+        char op[2]; int a, b;
         scanf("%s%d%d", op, &a, &b);
-        if(*op == 'M') p[find(a)] = find(b); //树a的root的值由自身改为树b的root(子节点),此时路劲压缩  (树a的所有结点的索引都将改为树b的root) 
-        if(*op == 'Q'){
+        if(*op == 'M') p[find(a)] = find(b);
+        if(*op == 'Q')
+        {
             if(find(a) == find(b)) puts("Yes");
             else puts("No");
         }
