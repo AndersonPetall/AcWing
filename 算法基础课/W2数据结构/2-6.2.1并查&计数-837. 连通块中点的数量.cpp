7f4e5023853f537630cafdfa39ad12b8609d2194
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-07-13 12:43:13 -0500

    Update 2-6.2.1并查&计数-837. 连通块中点的数量.cpp

--- 算法基础课/W2数据结构/2-6.2.1并查&计数-837. 连通块中点的数量.cpp
+++ 算法基础课/W2数据结构/2-6.2.1并查&计数-837. 连通块中点的数量.cpp
@@ -2,25 +2,25 @@
 #include<iostream>
 using namespace std;
 const int N = 1e5 + 10;
-int n, m, p[N], count[N];
-int find(int x){  //初始化,也可以从0开始
-    if(p[x] != x) p[x] = find(p[x]);
-    return p[x];
+int p[N], count[N];
+int find(int idx){
+    if(idx != p[idx]) p[idx] = find(p[idx]);
+    return p[idx];
 }
 int main(){
-    scanf("%d%d", &n , &m);
-    for(int i = 1; i <= n; ++i){
+    int n, m; scanf("%d%d", &n, &m);
+    for(int i = 0; i < n; ++i){
         p[i] = i;
         count[i] = 1;
     }
     while(m--){
-        string op; cin >> op;
-        int a, b;
+        string op;int a, b; cin >> op;
         if(op == "C"){
             scanf("%d%d", &a, &b);
-            if(find(a) != find(b)){
-                count[find(b)] += count[find(a)];  //先加后合并,否则root会多计一次
-                p[find(a)] = find(b);
+            a = find(a), b = find(b);
+            if(a != b){
+                p[a] = b;
+                count[b] += count[a];
             }
         }
         if(op == "Q1"){
@@ -30,11 +30,12 @@ int main(){
         }
         if(op == "Q2"){
             scanf("%d", &a);
-            cout << count[find(a)] <<endl;
+            printf("%d\n", count[find(a)]);
         }
     }
     return 0;
 }
+
 
 
 
@@ -58,10 +59,9 @@ int main(){
         string op;int a, b; cin >> op;
         if(op == "C"){
             scanf("%d%d", &a, &b);
-            a = find(a), b = find(b);
             if(find(a) != find(b)){
-                p[a] = b;
-                count[b] += count[a];
+            count[find(b)] += count[find(a)];
+            p[find(a)] = find(b);
             }
         }
         if(op == "Q1"){
