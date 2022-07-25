501f8ce5f22037ad989109ff293c66c43c999a90
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-07-25 13:08:12 -0500

    Update 3-3.1树与图的深度优先遍历_3-3.1树与图的深度优先遍历_846. 树的重心.cpp

--- 算法基础课/W3搜索与图论/3-3.1树与图的深度优先遍历_3-3.1树与图的深度优先遍历_846. 树的重心.cpp
+++ 算法基础课/W3搜索与图论/3-3.1树与图的深度优先遍历_3-3.1树与图的深度优先遍历_846. 树的重心.cpp
@@ -1,1 +1,121 @@
+//EDITION1  状态数组为int型    sum = 1(当前结点计入结点总数)
+#include<iostream>
+#include<cstring>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+const int N = 1e5 + 10, M = N * 2;
+int h[N], e[M], ne[M], idx, st[N];
+int n, ans = N;
+void add(int a, int b){
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
+int dfs(int u){
+    st[u] = 1;
+    int size = 0, sum = 1;
+    for(int i = h[u]; i != -1; i = ne[i]){
+        int j = e[i];
+        if(!st[j]){
+            int s = dfs(j);
+            size = max(size, s);
+            sum += s;
+        }
+    }
+    size = max(size, n - sum);
+    ans = min(ans, size);
+    return sum;
+}
+int main(){
+    scanf("%d", &n);
+    memset(h, -1, sizeof h);
+    for(int i = 0; i < n; ++i){
+        int a, b; scanf("%d%d", &a, &b);
+        add(a, b), add(b, a);
+    }
+    dfs(1);
+    printf("%d\n", ans);
+    return 0;
+}
 
+
+
+//EDITION2  状态数组为int型    sum = 0(当前结点不计入结点总数)
+#include<iostream>
+#include<cstring>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+const int N = 1e5 + 10, M = N * 2;
+int h[N], e[M], ne[M], idx, st[N];
+int n, ans = N;
+void add(int a, int b){
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
+int dfs(int u){
+    st[u] = 1;
+    int size = 0, sum = 0;
+    for(int i = h[u]; i != -1; i = ne[i]){
+        int j = e[i];
+        if(!st[j]){
+            int s = dfs(j);
+            size = max(size, s);
+            sum += s;
+        }
+    }
+    size = max(size, n - sum - 1);
+    ans = min(ans, size);
+    return sum + 1;
+}
+int main(){
+    scanf("%d", &n);
+    memset(h, -1, sizeof h);
+    for(int i = 0; i < n; ++i){
+        int a, b; scanf("%d%d", &a, &b);
+        add(a, b), add(b, a);
+    }
+    dfs(1);
+    printf("%d\n", ans);
+    return 0;
+}
+
+
+
+//EDITION3  状态数组为bool型    sum = 1(当前结点计入结点总数)
+#include<iostream>
+#include<cstring>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+const int N = 1e5 + 10, M = N * 2;
+int h[N], e[M], ne[M], idx;
+int n, ans = N;
+bool st[N];
+void add(int a, int b){
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
+int dfs(int u){
+    st[u] = true;
+    int size = 0, sum = 0;
+    for(int i = h[u]; i != -1; i = ne[i]){
+        int j = e[i];
+        if(!st[j]){
+            int s = dfs(j);
+            size = max(size, s);
+            sum += s;
+        }
+    }
+    size = max(size, n - sum - 1);
+    ans = min(ans, size);
+    return sum + 1;
+}
+int main(){
+    scanf("%d", &n);
+    memset(h, -1, sizeof h);
+    for(int i = 0; i < n; ++i){
+        int a, b; scanf("%d%d", &a, &b);
+        add(a, b), add(b, a);
+    }
+    dfs(1);
+    printf("%d\n", ans);
+    return 0;
+}
