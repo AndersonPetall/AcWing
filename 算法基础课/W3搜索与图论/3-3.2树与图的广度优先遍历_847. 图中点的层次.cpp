33f459aa9a139b24668fa19729bdbb35df9c21a9
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-07-26 08:37:02 -0500

    Create 3-3.2树与图的广度优先遍历_847. 图中点的层次.cpp

---
+++ 算法基础课/W3搜索与图论/3-3.2树与图的广度优先遍历_847. 图中点的层次.cpp
@@ -0,0 +1,43 @@
+//EDITION 1   queue
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<algorithm>
+#include<queue>
+using namespace std;
+const int N = 1e5 + 10;
+int h[N], e[N], ne[N], idx, d[N];
+int n, m;
+void add(int a, int b){
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
+int bfs(){
+    memset(d, -1, sizeof d); d[1] = 0;
+    queue<int> q; q.push(1);
+    while(q.size()){
+        int t = q.front(); q.pop();
+        for(int i = h[t]; i != -1; i = ne[i]){
+            int j = e[i];
+            if(d[j] == -1){
+                d[j] = d[t] + 1;
+                q.push(j);
+            }
+        }
+    }
+    return d[n];
+}
+int main(){
+    scanf("%d%d", &n, &m);
+    memset(h, -1, sizeof h);
+    for(int i = 0; i < m; ++i){
+        int a, b; scanf("%d%d", &a, &b);
+        add(a, b);
+    }
+    printf("%d", bfs());
+    return 0;
+}
+
+
+
+
+//EDITION 2   数组模拟queue
