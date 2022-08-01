609ead7207ab35143c1109d44a61ba942ec60479
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-08-01 09:35:18 -0500

    Create 3-4.1.1朴素Dijkstra算法_849. Dijkstra求最短路 I.cpp

---
+++ 算法基础课/W3搜索与图论/3-4.1.1朴素Dijkstra算法_849. Dijkstra求最短路 I.cpp
@@ -0,0 +1,32 @@
+#include<cstring>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+const int N = 510;
+int g[N][N], dis[N];
+int n, m;
+bool st[N];
+int dijkstra(){
+    memset(dis, 0x3f, sizeof dis);
+    dis[1] = 0;
+    for(int i = 0; i < n; ++i){
+        int t = -1;
+        for(int j = 1; j <=n; ++j)
+            if(!st[j] && (t == -1 || dis[t] > dis[j])) t = j;
+        st[t] = true;
+        for(int j = 1; j <= n; ++j)
+            dis[j] = min(dis[j], dis[t] + g[t][j]);
+    }
+    if(dis[n] == 0x3f3f3f3f) return -1;
+    return dis[n];
+}
+int main(){
+    scanf("%d%d", &n, &m);
+    memset(g, 0x3f, sizeof g);
+    while(m--){
+        int a, b, c; scanf("%d%d%d", &a, &b, &c);
+        g[a][b] = min(g[a][b], c);
+    }
+    printf("%d\n", dijkstra());
+    return 0;
+}
