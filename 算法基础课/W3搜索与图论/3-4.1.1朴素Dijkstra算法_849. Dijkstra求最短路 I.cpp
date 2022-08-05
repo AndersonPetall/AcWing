705d936474a2e78f7c1054ed4aa0f61c78508fb0
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-08-05 07:47:57 -0500

    Update 3-4.1.1朴素Dijkstra算法_849. Dijkstra求最短路 I.cpp

--- 算法基础课/W3搜索与图论/3-4.1.1朴素Dijkstra算法_849. Dijkstra求最短路 I.cpp
+++ 算法基础课/W3搜索与图论/3-4.1.1朴素Dijkstra算法_849. Dijkstra求最短路 I.cpp
@@ -29,3 +29,42 @@ int main(){
     printf("%d\n", dijkstra());
     return 0;
 }
+
+
+
+//EDITION 2 邻接表储存边
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+const int N = 510, M = 1e5 + 10;
+int e[M], ne[M], w[M], idx, h[N], dis[N];
+int n, m;
+bool st[N];
+void add(int a, int b, int c){
+    e[idx] = b, w[idx] = c, ne[idx] = h[a], h[a] = idx++;
+}
+int dijkstra(){
+    memset(dis, 0x3f, sizeof dis); dis[1] = 0;
+    for(int i = 0; i < n; ++i){
+        int t = -1;
+        for(int j = 1; j <= n; ++j)
+            if(!st[j] &&(t == -1 || dis[j] < dis[t])) t = j;
+        st[t] = true;
+        for(int j = h[t]; j != -1 ; j = ne[j]){
+            int ver = e[j];
+            dis[ver] = min(dis[ver], dis[t] + w[j]);
+        }
+    }
+    return dis[n] == 0x3f3f3f3f ? -1 : dis[n];
+}
+int main(){
+    scanf("%d%d", &n, &m);
+    memset(h, -1, sizeof h);
+    while(m--){
+        int a, b, c; scanf("%d%d%d", &a, &b, &c);
+        add(a, b, c);
+    }
+    printf("%d\n", dijkstra());
+    return 0;
+}
