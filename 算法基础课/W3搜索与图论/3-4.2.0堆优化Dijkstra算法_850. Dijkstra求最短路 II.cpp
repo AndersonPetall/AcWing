c21165a31a5f31a4d616948789918df0ec94f672
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-08-05 08:10:47 -0500

    Update 3-4.2.0堆优化Dijkstra算法_850. Dijkstra求最短路 II.cpp

--- 算法基础课/W3搜索与图论/3-4.2.0堆优化Dijkstra算法_850. Dijkstra求最短路 II.cpp
+++ 算法基础课/W3搜索与图论/3-4.2.0堆优化Dijkstra算法_850. Dijkstra求最短路 II.cpp
@@ -1,3 +1,50 @@
+//理解版本
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<queue>
+using namespace std;
+typedef pair<int, int> PII;
+const int N = 1e6 + 10;
+int e[N], ne[N], w[N], idx, h[N], dis[N];
+int n, m;
+bool st[N];
+void add(int a, int b, int c){
+    e[idx] = b, w[idx] = c, ne[idx] = h[a], h[a] = idx++;
+}
+int dijkstra(){
+    memset(dis, 0x3f, sizeof dis); dis[1] = 0;
+    priority_queue<PII, vector<PII>, greater<PII>> heap;
+    heap.push({dis[1], 1});
+    while(heap.size()){
+        auto t = heap.top(); heap.pop();
+        int pointIdx = t.second, distance = t.first;
+        if(st[pointIdx]) continue;
+        st[pointIdx] = true;
+        for(int i = h[pointIdx]; i != -1; i = ne[i]){
+            int subPointIdx = e[i];
+            if(dis[subPointIdx] > dis[pointIdx] + w[i]){
+                dis[subPointIdx] = dis[pointIdx] + w[i];
+                heap.push({dis[subPointIdx], subPointIdx});
+            }
+        }
+    }
+    return dis[n] == 0x3f3f3f3f ? -1 : dis[n];
+}
+int main(){
+    scanf("%d%d", &n, &m);
+    memset(h, -1, sizeof h); 
+    while(m--){
+        int a, b, c; scanf("%d%d%d", &a, &b, &c);
+        add(a, b, c);
+    }
+    printf("%d\n", dijkstra());
+    return 0;
+}
+
+
+
+//原版
 #include<cstring>
 #include<iostream>
 #include<algorithm>
