894d6a310398aefd19fad7df3c8db2e8779b7d36
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-08-06 08:12:54 -0500

    Update 2-3.1.1队列-829. 模拟队列.cpp

--- 算法基础课/W2数据结构/2-3.1.1队列-829. 模拟队列.cpp
+++ 算法基础课/W2数据结构/2-3.1.1队列-829. 模拟队列.cpp
@@ -1,20 +1,40 @@
-//EDITION 1   一个index
+//EDITION 1   top = 0
 #include<iostream>
 using namespace std;
 const int N = 1e5 + 10;
-int m, stack[N], idx = -1;
+int m, stack[N], top;
 int main(){
     scanf("%d", &m);
     while(m--){
         string s; cin >> s;
         if(s == "push"){
             int a; cin >> a;
-            stack[++idx] = a;
+            stack[top++] = a;
         }
-        if(s == "pop") --idx;
-        if(s == "query")  printf("%d\n", stack[0]);
-        if(s == "empty") cout << (idx == -1 ? "YES" : "NO") << endl;
-        
+        if(s == "pop") --top;
+        if(s == "empty") cout << (top == 0 ? "YES" : "NO") << endl;
+        if(s == "query") printf("%d\n", stack[top - 1]);
+    }
+    return 0;
+}
+
+
+//EDITION 2   top = -1
+#include<iostream>
+using namespace std;
+const int N = 1e5 + 10;
+int m, stack[N], top = -1;
+int main(){
+    scanf("%d", &m);
+    while(m--){
+        string s; cin >> s;
+        if(s == "push"){
+            int a; cin >> a;
+            stack[++top] = a;
+        }
+        if(s == "pop") --top;
+        if(s == "empty") cout << (top == -1 ? "YES" : "NO") << endl;
+        if(s == "query") printf("%d\n", stack[top]);
     }
     return 0;
 }
