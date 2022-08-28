87edef0f291bb05c6a3b1d2e69291417065c49c6
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-08-28 12:31:02 -0400

    Update 3-5.1.1朴素Prim算法_858. Prim算法求最小生成树.cpp

--- 算法基础课/W3搜索与图论/3-5.1.1朴素Prim算法_858. Prim算法求最小生成树.cpp
+++ 算法基础课/W3搜索与图论/3-5.1.1朴素Prim算法_858. Prim算法求最小生成树.cpp
@@ -12,7 +12,7 @@ int prim(){
         int pointIdx = -1;
         for(int j = 1; j <= n; ++j)
             if(!st[j] &&(pointIdx == -1 || dis[pointIdx] > dis[j])) pointIdx = j;
-        if(i && dis[pointIdx] == INF) return INF;
+        if(i && dis[pointIdx] == INF) return INF; //不是第一个点且距离为正无穷: 无法构建成树
         if(i) res += dis[pointIdx];
         st[pointIdx] = true;
         for(int j = 1; j <= n; ++j)dis[j] = min(dis[j], g[pointIdx][j]);
