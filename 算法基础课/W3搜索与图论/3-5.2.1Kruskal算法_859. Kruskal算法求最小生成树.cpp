cdb31f49d2d0fd4f4f03795165f1ebefaef0adc3
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-08-29 12:21:06 -0400

    Update 3-5.2.1Kruskal算法_859. Kruskal算法求最小生成树.cpp

--- 算法基础课/W3搜索与图论/3-5.2.1Kruskal算法_859. Kruskal算法求最小生成树.cpp
+++ 算法基础课/W3搜索与图论/3-5.2.1Kruskal算法_859. Kruskal算法求最小生成树.cpp
@@ -1,3 +1,54 @@
+//EDITION 1   
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+const int N = 1e5 + 10, M = 2e5 + 10, INF = 0x3f3f3f3f;
+int n, m, tree[N];
+struct Edge{
+    int a, b, w;
+    bool operator<(const Edge &W)const{
+        return w < W.w;
+    }
+}edges[M];
+int find(int x){
+    if(tree[x] != x) tree[x] = find(tree[x]);
+    return tree[x];
+}
+int kruskal(){
+    int res = 0, cnt = 0;
+    for(int i = 1; i <= m; ++i){
+        int a = find(edges[i].a), b = find(edges[i].b);
+        if(a != b){
+            tree[a] = b;
+            res += edges[i].w;
+            ++cnt;
+        }
+    }
+    if(cnt == n -1 )return res;
+    return INF;
+}
+int main(){
+    scanf("%d%d", &n, &m);
+    for(int i = 1; i <= m; ++i){
+        int a, b, w; scanf("%d%d%d", &a, &b, &w);
+        edges[i] = {a, b, w};
+    }
+    sort(edges + 1, edges + m + 1);
+    for(int i = 1; i <= n; ++i) tree[i] = i;
+    int t = kruskal();
+    t == INF ? puts("impossible") : printf("%d\n", t);
+    return 0;
+}
+
+
+
+
+
+
+
+
+//EDITION 2
 #include<iostream>
 #include<cstring>
 #include<algorithm>
