0a87ad9e8423ec50a90cb90896bfe04ee06de2a1
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-08-30 14:07:43 -0400

    Create 3-6.1.1染色法_860. 染色法判定二分图.cpp

---
+++ 算法基础课/W3搜索与图论/3-6.1.1染色法_860. 染色法判定二分图.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+const int N = 1e5 + 10, M = 2 * N;
+int n, m;
+int h[N], e[M], ne[M], idx;
+int color[N];
+void add(int a, int b){
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
+bool dfs(int u, int c){
+    color[u] = c;
+    for(int i = h[u]; i != -1; i = ne[i]){
+        int j = e[i];
+        if(!color[j]){
+            if(!dfs(j, 3-c)) return false;
+        }
+        else if(color[j] == c) return false;
+    }
+    return true;
+}
+int main(){
+    scanf("%d%d", &n, &m);
+    memset(h, -1, sizeof h);
+    while(m--){
+        int a, b; scanf("%d%d", &a, &b);
+        add(a, b); add(b, a);
+    }
+    bool flag = true;
+    for(int i = 1; i <= n; ++i){
+        if(!color[i]){
+            if(!dfs(i, 1)){
+                flag = false;
+                break;
+            }
+        }
+    }
+    flag == true ? puts("Yes") : puts("No");
+    return 0;
+}
