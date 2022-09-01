cd59bed57ea71b71b1c1f6070d9981b353fc0fca
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-09-01 15:54:21 -0400

    Update 3-6.1.1染色法_860. 染色法判定二分图.cpp

--- 算法基础课/W3搜索与图论/3-6.1.1染色法_860. 染色法判定二分图.cpp
+++ 算法基础课/W3搜索与图论/3-6.1.1染色法_860. 染色法判定二分图.cpp
@@ -78,3 +78,48 @@ int main(){
     flag == true ? puts("Yes") : puts("No");
     return 0;
 }
+
+
+
+//EDITION 3  BFS self
+#include<iostream>
+#include<cstring>
+#include<queue>
+using namespace std;
+const int N = 1e5 + 10, M = 2 * N;
+int n, m;
+int h[N], e[M], ne[M], idx;
+int color[N];
+void add(int a, int b){
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
+bool bfs(int pointIdx){
+    color[pointIdx] = 1;
+    queue<int> q; q.push(pointIdx);
+    while(q.size()){
+        int headPoint = q.front(); q.pop();
+        for(int i = h[headPoint]; i != -1; i = ne[i]){
+            int linkedPoint = e[i];
+            if(!color[linkedPoint]) color[linkedPoint] = 3 - color[headPoint], q.push(linkedPoint);
+            else if(color[linkedPoint] == color[headPoint]) return false;
+        }
+    }
+    return true;
+}
+int main(){
+    scanf("%d%d", &n, &m);
+    memset(h, -1, sizeof h);
+    while(m--){
+        int a, b; scanf("%d%d", &a, &b);
+        add(a, b); add(b, a);
+    }
+    bool flag = true;
+    for(int i = 1; i <= n; ++i){
+        if(!color[i] && !bfs(i)){
+            flag = false;
+            break;
+        }
+    }
+    flag ? puts("Yes") : puts("No");
+    return 0;
+}
