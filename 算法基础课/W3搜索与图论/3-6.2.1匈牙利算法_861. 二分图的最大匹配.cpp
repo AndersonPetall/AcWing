73abf055971fc8aca66bc2be3d639e9bcb8140d0
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-09-02 08:20:34 -0400

    Update 3-6.2.1匈牙利算法_861. 二分图的最大匹配.cpp

--- 算法基础课/W3搜索与图论/3-6.2.1匈牙利算法_861. 二分图的最大匹配.cpp
+++ 算法基础课/W3搜索与图论/3-6.2.1匈牙利算法_861. 二分图的最大匹配.cpp
@@ -1,3 +1,48 @@
+//EDITION 1 self
+#include<iostream>
+#include<cstring>
+using namespace std;
+const int N = 510, M = 1e5 + 10;
+int n1, n2, m;
+int h[N], e[M], ne[M], idx, match[N];
+bool st[N];
+void add(int a, int b){
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
+bool find(int pointIdx){
+    for(int i = h[pointIdx]; i != -1; i = ne[i]){
+        int N2 = e[i];
+        if(!st[N2]){
+            st[N2] = true;
+            if(match[N2] == 0 || find(match[N2])){
+                match[N2] = pointIdx;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+int Hungarian(){
+    int res = 0;
+    for(int i = 1; i <= n1; ++i){
+        memset(st, false, sizeof st);
+        if(find(i)) ++res;
+    }
+    return res;
+}
+int main(){
+    scanf("%d%d%d", &n1, &n2, &m);
+    memset(h, -1, sizeof h);
+    while(m--){
+        int a, b; scanf("%d%d", &a, &b);
+        add(a, b);
+    }
+    printf("%d\n", Hungarian());
+    return 0;
+}
+
+
+//EDITION ACwing
 #include<iostream>
 #include<cstring>
 #include<algorithm>
