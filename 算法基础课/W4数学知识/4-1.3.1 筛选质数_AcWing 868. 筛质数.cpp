e0fd5c67ed6584e641241fbe8b5768ea9c4c4471
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-09-06 15:00:32 -0400

    Update 4-1.3.1 筛选质数_AcWing 868. 筛质数.cpp

--- 算法基础课/W4数学知识/4-1.3.1 筛选质数_AcWing 868. 筛质数.cpp
+++ 算法基础课/W4数学知识/4-1.3.1 筛选质数_AcWing 868. 筛质数.cpp
@@ -40,3 +40,24 @@ int main(){
     printf("%d\n", get_primes(n));
     return 0;
 }
+
+
+
+#include<iostream>
+using namespace std;
+const int N = 1e6 + 10;
+bool st[N];
+int get_primes(int x){
+    int cnt = 0;
+    for(int i = 2; i <= x; ++i)
+        if(!st[i]) {
+            ++cnt;
+            for(int j = i + i; j <= x; j += i) st[j] = true;
+        }
+    return cnt++;
+}
+int main(){
+    int n; cin >> n;
+    printf("%d\n", get_primes(n));
+    return 0;
+}
