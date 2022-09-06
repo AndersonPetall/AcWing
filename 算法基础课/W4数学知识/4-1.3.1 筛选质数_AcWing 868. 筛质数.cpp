8f884c196895ce859cab26d6da65f4cdf8331214
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-09-06 15:17:12 -0400

    Update 4-1.3.1 筛选质数_AcWing 868. 筛质数.cpp

--- 算法基础课/W4数学知识/4-1.3.1 筛选质数_AcWing 868. 筛质数.cpp
+++ 算法基础课/W4数学知识/4-1.3.1 筛选质数_AcWing 868. 筛质数.cpp
@@ -1,4 +1,4 @@
-//
+//朴素版
 #include<iostream>
 using namespace std;
 const int N = 1e6 + 10;
@@ -20,7 +20,8 @@ int main(){
 }
 
 
-//
+
+//诶氏筛法
 #include<iostream>
 using namespace std;
 const int N = 1e6 + 10;
@@ -43,6 +44,7 @@ int main(){
 
 
 
+//诶氏筛法 improvement
 #include<iostream>
 using namespace std;
 const int N = 1e6 + 10;
@@ -55,6 +57,31 @@ int get_primes(int x){
             for(int j = i + i; j <= x; j += i) st[j] = true;
         }
     return cnt++;
+}
+int main(){
+    int n; cin >> n;
+    printf("%d\n", get_primes(n));
+    return 0;
+}
+
+
+
+//线性筛法 
+#include<iostream>
+using namespace std;
+const int N = 1e6 + 10;
+int primes[N];
+bool st[N];
+int get_primes(int x){
+    int cnt = 0;
+    for(int i = 2; i <= x; ++i){
+        if(!st[i]) primes[cnt++] = i;
+        for(int j = 0; primes[j] <= x / i; ++j){
+            st[primes[j] * i] = true;
+            if(i % primes[j] == 0) break;
+        }
+    }
+    return cnt;
 }
 int main(){
     int n; cin >> n;
