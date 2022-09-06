5ffe65b3109116b684261d2b6fa223b1f9e95150
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-09-06 00:32:23 -0400

    Update 4-1.2.1 分解质因数_867. 分解质因数.cpp

--- 算法基础课/W4数学知识/4-1.2.1 分解质因数_867. 分解质因数.cpp
+++ 算法基础课/W4数学知识/4-1.2.1 分解质因数_867. 分解质因数.cpp
@@ -1,26 +1,4 @@
-#include<iostream>
-using namespace std;
-void divide(int x){
-    for(int i = 2; i <= x/i; ++i)
-        if(x % i == 0){
-            int s = 0; 
-            while(x % i == 0) x /= i, ++s;
-            cout << i << ' ' << s << endl;
-        }
-    if(x > 1) cout << x << ' ' << 1 << endl;
-    cout << endl;
-}
-int main(){
-    int n; cin >> n;
-    while(n--){
-        int x; cin >>x;
-        divide(x);
-    }
-    return 0;
-}
-
-
-
+//EDITION 1
 #include<iostream>
 using namespace std;
 void divide(int x){
@@ -32,6 +10,29 @@ void divide(int x){
         }
     if(x > 1)printf("%d %d\n", x, 1);
     puts("");
+}
+int main(){
+    int n; cin >> n;
+    while(n--){
+        int x; cin >>x;
+        divide(x);
+    }
+    return 0;
+}
+
+
+//EDITION 2
+#include<iostream>
+using namespace std;
+void divide(int x){
+    for(int i = 2; i <= x/i; ++i)
+        if(x % i == 0){
+            int s = 0; 
+            while(x % i == 0) x /= i, ++s;
+            cout << i << ' ' << s << endl;
+        }
+    if(x > 1) cout << x << ' ' << 1 << endl;
+    cout << endl;
 }
 int main(){
     int n; cin >> n;
