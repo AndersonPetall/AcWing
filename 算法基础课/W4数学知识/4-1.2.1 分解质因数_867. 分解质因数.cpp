ea3cad16c757d19e1902f7aa09560b71084e3e66
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-09-05 23:23:30 -0400

    Update 4-1.2.1 分解质因数_867. 分解质因数.cpp

--- 算法基础课/W4数学知识/4-1.2.1 分解质因数_867. 分解质因数.cpp
+++ 算法基础课/W4数学知识/4-1.2.1 分解质因数_867. 分解质因数.cpp
@@ -1,1 +1,43 @@
+#include<iostream>
+using namespace std;
+void divide(int x){
+    for(int i = 2; i <= x/i; ++i)
+        if(x % i == 0){
+            int s = 0; 
+            while(x % i == 0) x /= i, ++s;
+            cout << i << ' ' << s << endl;
+        }
+    if(x > 1) cout << x << ' ' << 1 << endl;
+    cout << endl;
+}
+int main(){
+    int n; cin >> n;
+    while(n--){
+        int x; cin >>x;
+        divide(x);
+    }
+    return 0;
+}
 
+
+
+#include<iostream>
+using namespace std;
+void divide(int x){
+    for(int i = 2; i <= x; ++i)
+        if(x % i == 0){
+            int s = 0;
+            while(x % i == 0) x /= i, ++s;
+            printf("%d %d\n", i, s);
+        }
+    if(x > 1)printf("%d %d\n", x, 1);
+    puts("");
+}
+int main(){
+    int n; cin >> n;
+    while(n--){
+        int x; cin >>x;
+        divide(x);
+    }
+    return 0;
+}
