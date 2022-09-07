70b38d33b091daccaa52791862014f03c889a9aa
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-09-07 16:51:43 -0400

    Update 4-2.2.1 约数之和_871. 约数之和.cpp

--- 算法基础课/W4数学知识/4-2.2.1 约数之和_871. 约数之和.cpp
+++ 算法基础课/W4数学知识/4-2.2.1 约数之和_871. 约数之和.cpp
@@ -1,1 +1,23 @@
-
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+using LL = long long;
+const int mod = 1e9 + 7;
+int main(){
+    int n; scanf("%d", &n);
+    unordered_map<int, int> primes;
+    while(n--){
+        int x; scanf("%d", &x);
+        for(int i = 2; i <= x/i; ++i)
+            while(x % i == 0) x /= i, primes[i]++;
+        if(x > 1) primes[x]++;
+    }
+    LL res = 1;
+    for(auto x : primes){
+        LL a = x.first, b = x.second, total = 1;
+        while(b--) total = (total * a + 1) % mod;
+        res = res * total % mod;
+    }
+    printf("%lld\n", res);
+    return 0;
+}
