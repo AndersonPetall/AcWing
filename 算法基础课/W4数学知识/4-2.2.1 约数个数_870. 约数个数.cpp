30b438d42bc7594efa1642b258165918177b2269
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-09-07 16:33:21 -0400

    Update 4-2.2.1 约数个数_870. 约数个数.cpp

--- 算法基础课/W4数学知识/4-2.2.1 约数个数_870. 约数个数.cpp
+++ 算法基础课/W4数学知识/4-2.2.1 约数个数_870. 约数个数.cpp
@@ -1,1 +1,18 @@
-
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+const int mod = 1e9 + 7;
+int main(){
+    int n; cin >> n;
+    unordered_map<int, int> primes;
+    while(n--){
+        int x; cin >> x;
+        for(int i = 2; i <= x/i; ++i)
+            while(x % i == 0) x /= i, primes[i] ++;
+        if(x > 1) primes[x]++;
+    }
+    long long res = 1;
+    for(auto x : primes) res = res * (x.second + 1) % mod;
+    printf("%lld\n", res);
+    return 0;
+}
