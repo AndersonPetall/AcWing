ce195defc5ea5204a07b879c40cb18cf7d48f153
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-09-09 21:39:35 -0400

    Update 4-2.3.1 最大公约数_872. 最大公约数.cpp

--- 算法基础课/W4数学知识/4-2.3.1 最大公约数_872. 最大公约数.cpp
+++ 算法基础课/W4数学知识/4-2.3.1 最大公约数_872. 最大公约数.cpp
@@ -1,3 +1,4 @@
+//EDITION 1  self
 #include<iostream>
 using namespace std;
 int gcd(int a, int b){
@@ -8,6 +9,20 @@ int main(){
     while(n--){
         int a, b; scanf("%d%d", &a, &b);
         printf("%d\n", gcd(a, b));
+    }
+    return 0;
+}
+
+
+//EDITION 2  STL自带的求最大公约数的函数__gcd(a,b)
+#include<bits/stdc++.h>
+using namespace std;
+int n,a,b;
+int main() {
+    cin >> n;
+    while(n--){
+        cin >> a >> b;
+        cout << __gcd(a,b) << endl;
     }
     return 0;
 }
