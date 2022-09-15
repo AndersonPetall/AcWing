bf3dd775927b48524aa95faffd4d2d070cce17d2
AndersonPetall <andersonpetall@gmail.com>
2022-09-15 10:41:26 -0400

    Update 4-4.3.1 拓展欧几里德算法_877. 扩展欧几里得算法.cpp

--- 算法基础课/W4数学知识/4-4.3.1 拓展欧几里德算法_877. 扩展欧几里得算法.cpp
+++ 算法基础课/W4数学知识/4-4.3.1 拓展欧几里德算法_877. 扩展欧几里得算法.cpp
@@ -7,7 +7,7 @@ int exgcd(int a, int b, int &x, int &y){
         return a;
     }
     int d = exgcd(b, a%b, y, x);  // x = &y, y = &x
-    y -= a/b*x;
+    y -= a/b*x;  // y = &x - a/b*&y = y - a/b*x
     return d;
 }
 int main(){
@@ -21,13 +21,12 @@ int main(){
 }
 
 
-
+//EDITION 理解版
 #include<iostream>
-#include<algorithm>
 using namespace std;
 int exgcd(int a, int b, int &x, int &y){
-    if(b==0){
-        x = 1, y = 0;
+    if(b == 0){
+        x = 1, y = 0; 
         return a;
     }
     int x1, y1, gcd;
@@ -38,30 +37,7 @@ int exgcd(int a, int b, int &x, int &y){
 int main(){
     int n; scanf("%d", &n);
     while(n--){
-        int a, b, x, y;scanf("%d%d", &a, &b);
-        exgcd(a, b, x, y);
-        printf("%d %d\n", x, y);
-    }
-    return 0;
-}
-
-
-
-#include<iostream>
-using namespace std;
-int exgcd(int a, int b, int &x, int &y){
-    if(b == 0){
-        x = 1, y = 0;
-        return a;
-    }
-    int gcd = exgcd(b, a%b, y, x);
-    y -= a/b*x;
-    return gcd;
-}
-int main(){
-    int n; scanf("%d", &n);
-    while(n--){
-        int a, b, x=0, y=0;; scanf("%d%d", &a, &b);
+        int a, b, x, y; scanf("%d%d", &a, &b);
         exgcd(a, b, x, y);
         printf("%d %d\n", x, y);
     }
