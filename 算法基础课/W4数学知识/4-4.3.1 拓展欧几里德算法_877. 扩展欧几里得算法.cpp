496dd012046bc546408dddbaaf0d066d95436319
AndersonPetall <andersonpetall@gmail.com>
2022-09-15 10:33:17 -0400

    Update 4-4.3.1 拓展欧几里德算法_877. 扩展欧几里得算法.cpp

--- 算法基础课/W4数学知识/4-4.3.1 拓展欧几里德算法_877. 扩展欧几里得算法.cpp
+++ 算法基础课/W4数学知识/4-4.3.1 拓展欧几里德算法_877. 扩展欧几里得算法.cpp
@@ -1,3 +1,27 @@
+//EDITION    精简版
+#include<iostream>
+using namespace std;
+int exgcd(int a, int b, int &x, int &y){
+    if(!b){
+        x = 1, y = 0;
+        return a;
+    }
+    int d = exgcd(b, a%b, y, x);  //
+    y -= a/b*x;
+    return d;
+}
+int main(){
+    int n; scanf("%d", &n);
+    while(n--){
+        int a, b, x, y; scanf("%d%d", &a, &b);
+        exgcd(a, b, x, y);
+        printf("%d %d\n", x, y);
+    }
+    return 0;
+}
+
+
+
 #include<iostream>
 #include<algorithm>
 using namespace std;
