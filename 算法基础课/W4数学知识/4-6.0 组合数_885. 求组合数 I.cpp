322441df426d5503cb8b5537953716f681061400
AndersonPetall <andersonpetall@gmail.com>
2022-12-21 13:48:27 -0500

    Create 4-6.0 组合数_885. 求组合数 I.cpp

---
+++ 算法基础课/W4数学知识/4-6.0 组合数_885. 求组合数 I.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+const int N = 2e3+10, mod=1e9+7;
+int c[N][N];
+void init(){
+    for(int i=0; i<N; ++i)for(int j=0;j<=i; ++j) !j ? c[i][j]=1:c[i][j]=(c[i-1][j]+c[i-1][j-1])%mod;
+}
+int main(){
+    int n; init();
+    scanf("%d",&n);
+    while(n--){
+        int a,b; scanf("%d%d",&a,&b);
+        printf("%d\n",c[a][b]);
+    }
+    return 0;
+}
