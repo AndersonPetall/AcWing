6bbe4e86aca5773daa0b7e63c1f4d6233a6c2120
AndersonPetall <andersonpetall@gmail.com>
2022-12-22 10:00:19 -0500

    Update 4-5.2.1 高斯消元_884. 高斯消元解异或线性方程组.cpp

--- 算法基础课/W4数学知识/4-5.2.1 高斯消元_884. 高斯消元解异或线性方程组.cpp
+++ 算法基础课/W4数学知识/4-5.2.1 高斯消元_884. 高斯消元解异或线性方程组.cpp
@@ -7,7 +7,7 @@ int gauss(){
     int c,r;
     for(c=0,r=0;c<n;++c){
         int t = r;
-        for(int i=r;i<n;++i)if(a[i][c])t=i;
+        for(int i=r;i<n;++i)if(a[i][c]){t=i;break;}
         if(!a[t][c])continue;
         for(int i=c;i<=n;++i)swap(a[r][i],a[t][i]);
         for(int i=r+1;i<n;++i)if(a[i][c])for(int j=n;j>=c;--j)a[i][j]^=a[r][j];
@@ -24,7 +24,6 @@ int main(){
     cin>>n;for(int i=0;i<n;++i)for(int j=0;j<n+1;++j)cin>>a[i][j];
     int t = gauss();
     if(t==0)for(int i=0;i<n;++i)cout<<a[i][n]<<endl;
-    else if(t==1)puts("Multiple sets of solutions");
-    else puts("No solution");
+    else t==1 ? puts("Multiple sets of solutions"):puts("No solution");
     return 0;
 }
