a8861305852b06bfcae2ff6517b93a2085566a6c
AndersonPetall <andersonpetall@gmail.com>
2022-12-25 09:50:24 -0500

    Update 4-5.1.1 高斯消元_883. 高斯消元解线性方程组.cpp

--- 算法基础课/W4数学知识/4-5.1.1 高斯消元_883. 高斯消元解线性方程组.cpp
+++ 算法基础课/W4数学知识/4-5.1.1 高斯消元_883. 高斯消元解线性方程组.cpp
@@ -50,21 +50,31 @@ int gauss(int n){
     int c,r;
     for(c=r=0;c<n;++c){
         int t = r;
-        
+        //寻找C列的最大值
         for(int i=r;i<n;++i)
             if(fabs(a[i][c])>fabs(a[t][c])) t=i;
+        //如果最大值为0,则意味该列无解(方程系数为0)
         if(fabs(a[t][c])<eps) continue;
+        //将当前列(列C)置换至未操作的方程组的最上层
         for(int i=c;i<=n;++i) swap(a[t][i],a[r][i]);
+        //最大值所在的行  将最大值系数变为1
         for(int i=n;i>=c;--i)a[r][i]/=a[r][c];
+        //利用最大值所在的行,将未操作的方程组的列C的系数变为0
         for(int i=r+1;i<n;++i)
             if(fabs(a[i][c])>eps)
                 for(int j=n;j>=c;--j)a[i][j]-=a[r][j]*a[i][c];
         ++r;
     }
+    //行数 < 列数(方程组中某几行方程系数为0)
     if(r<n){
-        for(int i=r;i<n;++i)if(fabs(a[i][n])>eps)return 2;
+        // 若存在 方程系数为0,但方程结果不为0
+        for(int i=r;i<n;++i)
+            //无解(0 = 非0 矛盾)
+            if(fabs(a[i][n])>eps)return 2;
+        //无穷解(0 = 0)
         return 1;
     }
+    //唯一解
     for(int i=n-1;i>=0;--i)for(int j=i+1;j<n;++j)a[i][n]-=a[i][j]*a[j][n];
     return 0;
 }
