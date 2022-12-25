6409beb9fb3b91c633655cf2fdb132719fcad329
AndersonPetall <andersonpetall@gmail.com>
2022-12-25 16:15:28 -0500

    Update 4-6.0 组合数_886. 求组合数 II.cpp

--- 算法基础课/W4数学知识/4-6.0 组合数_886. 求组合数 II.cpp
+++ 算法基础课/W4数学知识/4-6.0 组合数_886. 求组合数 II.cpp
@@ -1,29 +1,54 @@
 #include<iostream>
-#include<algorithm>
 using namespace std;
 using LL = long long;
-const int N = 1e5+10, mod = 1e9+7;
-int fact[N],infact[N];
-int qmi(int a,int k, int p){
-    int res = 1;
-     while(k){
-         if(k&1) res = (LL)res*a%p;
-         a = (LL)a*a%p;
-         k>>=1;
-     }
-     return res;
+const int N = 1e5+10,mod=1e9+7;
+LL fact[N],infact[N];
+LL qmi(LL a, LL b, LL c){
+    if(!b) return 1;
+    LL res = qmi(a*a%c,b>>1,c);
+    return b&1 ? res*a%c : res;
 }
 int main(){
-    fact[0] = infact[0]=1;
-    for(int i = 1; i < N; ++i){
+    fact[0]=infact[0]=1;
+    for(int i=1;i<N;++i){
+        fact[i]=fact[i-1]*i%mod;
+        infact[i]=infact[i-1]*qmi(i,mod-2,mod)%mod;
+    }
+    int n;cin>>n;
+    while(n--){
+        int a,b;scanf("%d%d",&a,&b);
+        printf("%lld\n",fact[a]*infact[b]%mod*infact[a-b]%mod);
+    }
+    
+    return 0;
+}
+
+
+
+
+
+
+#include<iostream>
+using namespace std;
+using LL = long long;
+const int N = 1e5+10,mod=1e9+7;
+int fact[N],infact[N];
+LL qmi(LL a, LL b, LL c){
+    if(!b) return 1;
+    LL res = qmi(a*a%c,b>>1,c);
+    return b&1 ? res*a%c : res;
+}
+int main(){
+    fact[0]=infact[0]=1;
+    for(int i=1;i<N;++i){
         fact[i]=(LL)fact[i-1]*i%mod;
         infact[i]=(LL)infact[i-1]*qmi(i,mod-2,mod)%mod;
     }
-    int n;
-    scanf("%d",&n);
+    int n;cin>>n;
     while(n--){
         int a,b;scanf("%d%d",&a,&b);
         printf("%lld\n",(LL)fact[a]*infact[b]%mod*infact[a-b]%mod);
     }
+    
     return 0;
 }
