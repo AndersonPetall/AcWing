beec227f6e82f74824b14dbe74be9106bd0c3efa
AndersonPetall <andersonpetall@gmail.com>
2022-12-25 14:47:53 -0500

    Update 4-5.2.1 高斯消元_884. 高斯消元解异或线性方程组.cpp

--- 算法基础课/W4数学知识/4-5.2.1 高斯消元_884. 高斯消元解异或线性方程组.cpp
+++ 算法基础课/W4数学知识/4-5.2.1 高斯消元_884. 高斯消元解异或线性方程组.cpp
@@ -23,7 +23,44 @@ int gauss(){
 int main(){
     cin>>n;for(int i=0;i<n;++i)for(int j=0;j<n+1;++j)cin>>a[i][j];
     int t = gauss();
-    if(t==0)for(int i=0;i<n;++i)cout<<a[i][n]<<endl;
+    if(t==0)for(int i=0;i<n;++i)printf("%d\n",a[i][n]);
+    else t==1 ? puts("Multiple sets of solutions"):puts("No solution");
+    return 0;
+}
+
+
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+const int N = 110;
+int n, a[N][N];
+int gauss(){
+    int c,r;
+    for(c=r=0;c<n;++c){
+        int t = r;
+        //寻找最大值,找到即退出循环
+        for(int i=r;i<n;++i)
+            if(a[i][c]){t=i;break;}
+        if(!a[t][c])continue;
+        for(int i=c;i<=n;++i)swap(a[r][i],a[t][i]);
+        for(int i=r+1;i<n;++i)
+            if(a[i][c])
+                for(int j=n;j>=c;--j)a[i][j]^=a[r][j];
+        ++r;
+    }
+    if(r<n){
+        for(int i=r;i<n;++i)if(a[i][n])return 2;
+        return 1;
+    }
+    for(int i=n-1;i>=0;--i)
+        for(int j=i+1;j<n;++j)a[i][n]^=a[i][j]*a[j][n];
+    return 0;
+}
+int main(){
+    cin>>n;for(int i=0;i<n;++i)for(int j=0;j<n+1;++j)cin>>a[i][j];
+    int t = gauss();
+    if(t==0)for(int i=0;i<n;++i)printf("%d\n",a[i][n]);
     else t==1 ? puts("Multiple sets of solutions"):puts("No solution");
     return 0;
 }
