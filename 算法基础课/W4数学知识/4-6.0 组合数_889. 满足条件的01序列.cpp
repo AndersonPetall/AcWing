eaf4f487489694109dd6c20831d3c11dd26ba097
AndersonPetall <andersonpetall@gmail.com>
2022-12-26 15:53:08 -0500

    Update 4-6.0 组合数_889. 满足条件的01序列.cpp

--- 算法基础课/W4数学知识/4-6.0 组合数_889. 满足条件的01序列.cpp
+++ 算法基础课/W4数学知识/4-6.0 组合数_889. 满足条件的01序列.cpp
@@ -1,23 +1,42 @@
 #include<iostream>
+using namespace std;
+using LL = long long;
+const int mod = 1e9+7;
+LL qmi(LL a,LL b,LL c){
+    if(!b) return 1;
+    LL res = qmi(a*a%c,b>>1,c);
+    return b&1 ? res*a%c : res;
+}
+int main(){
+    int n; cin>>n;
+    LL res = 1;
+    for(int i=2*n;i>n;--i)res =res*i%mod;
+    for(int i=1;i<=n;++i)res=res*qmi(i,mod-2,mod)%mod;
+    printf("%lld\n",res*qmi(n+1,mod-2,mod)%mod);
+    return 0;
+}
+
+
+
+#include<iostream>
 #include<algorithm>
 using namespace std;
 const int N=1e5+10,mod=1e9+7;
 using LL = long long;
-int qmi(int a, int k, int p){
-    int res=1;
-    while(k){
-        if(k&1)res=(LL)res*a%p;
-        a=(LL)a*a%p;
-        k>>=1;
-    }
-    return res;
+LL qmi(LL a, LL b, LL c){
+    if(!b) return 1;
+    LL res = qmi(a*a%c,b>>1,c);
+    return b&1 ? res*a%c : res;
 }
 int main(){
     int n;cin>>n;
-    int a=n*2,b=n,res=1;
-    for(int i=a;i>a-b;--i)res=(LL)res*i%mod;
-    for(int i=1;i<=b;++i)res=(LL)res*qmi(i,mod-2,mod)%mod;
-    res=(LL)res*qmi(n+1,mod-2,mod)%mod;
+    LL res=1;
+    //(2*n)! / n!
+    for(int i=2*n;i>n;--i)res=res*i%mod;
+    //n! 关于mod的逆元
+    for(int i=1;i<=n;++i)res=res*qmi(i,mod-2,mod)%mod;
+    //n+1 关于mod的逆元
+    res=res*qmi(n+1,mod-2,mod)%mod;
     printf("%d\n",res);
     return 0;
 }
