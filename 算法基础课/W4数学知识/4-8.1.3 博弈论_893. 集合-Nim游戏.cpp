3538163abcf419a582cfb8ad1740c9a6875e413b
AndersonPetall <andersonpetall@gmail.com>
2023-01-01 09:52:22 -0500

    Update 4-8.1.3 博弈论_893. 集合-Nim游戏.cpp

--- 算法基础课/W4数学知识/4-8.1.3 博弈论_893. 集合-Nim游戏.cpp
+++ 算法基础课/W4数学知识/4-8.1.3 博弈论_893. 集合-Nim游戏.cpp
@@ -58,18 +58,50 @@ SG定理(Sprague-Grundy Theorem)
 
 #include<iostream>
 #include<cstring>
-#include<algorithm>
+#include<set>
+using namespace std;
+const int N = 1e2+10,M=1e4+10;
+int n,m, res=0,s[N],f[M];
+int sg(int x){
+    if(f[x]!=-1) return f[x];
+    set<int> S;
+    for(int i=0;i<m;++i){
+        if(x >= s[i]) S.insert(sg(x-s[i]));
+        else continue;
+    }
+    for(int i=0;;++i)if(!S.count(i)) return f[x] = i;
+}
+int main(){
+    cin>>m;
+    for(int i=0;i<m;++i)cin>>s[i];
+    cin>>n;
+    memset(f,-1,sizeof f);
+    for(int i=0;i<n;++i){
+        int x;cin>>x;
+        res ^=sg(x);
+    }
+    res ? puts("Yes") : puts("No");
+    return 0;
+}
+
+
+
+
+#include<iostream>
+#include<cstring>
 #include<set>
 using namespace std;
 const int N = 1e2+10,M=1e4+10;
 int n,m,s[N],f[M];//s存储的是可供选择的集合,f存储的是所有可能出现过的情况的sg值
+//SG()函数计算可以利用 2(2,5)+1(10) 以及 2(2,5)+1(1) 两个例子理解
 int sg(int x){
-    if(f[x]!=-1) return f[x]; //因为取石子数目的集合是已经确定了的,所以每个数的sg值也都是确定的,如果存储过了,直接返回即可
-    set<int> S;
+    if(f[x]!=-1) return f[x];//因为取石子数目的集合是已经确定了的,所以每个数的sg值也都是确定的,如果存储过了,直接返回即可
+    set<int> S;//set代表的是有序集合(注:因为在函数内部定义,所以下一次递归中的S不与本次相同)
     for(int i=0;i<m;++i){
-        if(x >= s[i]) S.insert(sg(x-s[i]));
+        if(x >= s[i]) S.insert(sg(x-s[i]));//递归调用, 先延申至边界计算边界的SG值,然后从边界往回计算出所有局面的SG值
+        else continue;
     }
-    for(int i=0;;++i)if(!S.count(i)) return f[x] = i;
+    for(int i=0;;++i)if(!S.count(i)) return f[x] = i;//第一个未出现的数即为SG函数(mex运算定义)
 }
 int main(){
     cin>>m;
