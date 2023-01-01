f3518cdbc30b23e6bfe500f7cb3d76b320205863
AndersonPetall <andersonpetall@gmail.com>
2023-01-01 09:53:31 -0500

    Create 4-8.1.4 博弈论_894. 拆分-Nim游戏.cpp

---
+++ 算法基础课/W4数学知识/4-8.1.4 博弈论_894. 拆分-Nim游戏.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cstring>
+#include<unordered_set>
+using namespace std;
+const int N=1e2+10;
+int n, f[N];
+unordered_set<int> S;
+int sg(int x){
+    if(f[x]!=-1) return f[x];
+    for(int i=0;i<x;++i)for(int j=0;j<=i;++j)S.insert(sg(i)^sg(j));
+    for(int i=0;;++i)if(!S.count(i))return f[x]=i;
+}
+int main(){
+    memset(f,-1,sizeof f);
+    cin>>n;
+    int res =0;
+    while(n--){
+        int x;cin>>x;
+        res ^=sg(x);
+    }
+    res ? puts("Yes") : puts("No");
+    return 0;
+}
