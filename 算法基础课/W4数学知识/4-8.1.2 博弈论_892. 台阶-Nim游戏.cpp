0465a9fbbdfe60680608cd3983db486cb479c6c0
AndersonPetall <andersonpetall@gmail.com>
2023-01-01 15:36:13 -0500

    Update 4-8.1.2 博弈论_892. 台阶-Nim游戏.cpp

--- 算法基础课/W4数学知识/4-8.1.2 博弈论_892. 台阶-Nim游戏.cpp
+++ 算法基础课/W4数学知识/4-8.1.2 博弈论_892. 台阶-Nim游戏.cpp
@@ -1,11 +1,3 @@
-问题关键: 只用考虑奇数台阶
-  当玩家1从偶数台阶i 移动一定数量到台阶i-1 的时候,玩家2可以从台阶i-1移动相同的数量到台阶i-2,相当于没有操作((i-2)-->0)
-  当玩家1从奇数台阶i 移动一定数量到台阶i-1 的时候, (i-1)-->0 玩家2 可能无法操作,即失败
-抽象举例: 所有奇数台阶为台阶1, 所有偶数台阶为台阶2
-  
-Reference Url:
-  https://www.cnblogs.com/lucky-light/p/16512304.html
-
 #include<iostream>
 using namespace std;
 int main(){
