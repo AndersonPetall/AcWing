423b452bf977c07006f3fd1ac931515dc40dfc6b
AndersonPetall <andersonpetall@gmail.com>
2023-01-07 23:53:38 -0500

    Update 4-4.4.中国剩余定理_204. 表达整数的奇怪方式.cpp

--- 算法基础课/W4数学知识/4-4.4.中国剩余定理_204. 表达整数的奇怪方式.cpp
+++ 算法基础课/W4数学知识/4-4.4.中国剩余定理_204. 表达整数的奇怪方式.cpp
@@ -1,3 +1,4 @@
+//EDITION self
 #include<iostream>
 using namespace std;
 using LL = long long;
@@ -17,8 +18,39 @@ int main(){
         LL a2,m2,k1,k2;scanf("%lld%lld",&a2,&m2);
         LL d = exgcd(a1,a2,k1,k2);
         if((m2-m1)%d){puts("-1");return 0;}
-        LL t = abs(a2/d);
+        LL t = a2/d;
         k1 = (k1*(m2-m1)/d%t+t)%t;
+        m1=k1*a1+m1;
+        a1=abs(a1*a2/d);
+    }
+    printf("%lld\n",m1);
+    return 0;
+}
+
+
+//EDITION　ACwing
+#include<iostream>
+using namespace std;
+using LL = long long;
+LL exgcd(LL a, LL b, LL &x, LL &y){
+    if(!b){
+        x=1,y=0;
+        return a;
+    }
+    LL d = exgcd(b,a%b,y,x);
+    y-=a/b*x;
+    return d;
+}
+int main(){
+    int n; cin>>n; --n;
+    LL a1,m1; scanf("%lld%lld",&a1,&m1);
+    while(n--){
+        LL a2,m2,k1,k2;scanf("%lld%lld",&a2,&m2);
+        LL d = exgcd(a1,a2,k1,k2);
+        if((m2-m1)%d){puts("-1");return 0;}//判断是否有解
+        LL t = abs(a2/d);//abs为求绝对值的内置方程
+        k1 *= (m2-m1)/d;//求(m2-m1)下对应的k1值
+        k1 = (k1%t+t)%t;//k1 = k1 + k*a2/gcd(a1,a2)  求 a2/gcd(a1,a2) 范围下的特解
         m1=k1*a1+m1;
         a1=abs(a1*a2/d);
     }
