8dd820d6520767a28bc91f61a0b35bcfe98e0911
AndersonPetall <andersonpetall@gmail.com>
2023-01-25 09:23:30 -0500

    Update 5-1.1.0 背包问题_2. 01背包问题.cpp

--- 算法基础课/W5动态规划/5-1.1.0 背包问题_2. 01背包问题.cpp
+++ 算法基础课/W5动态规划/5-1.1.0 背包问题_2. 01背包问题.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 using namespace std;
 const int N = 1e3+10;
+int n,m,v[N],w[N],f[N][N];
+int main(){
+    cin>>n>>m;
+    for(int i=1;i<=n;++i)cin>>v[i]>>w[i];
+    for(int i=1;i<=n;++i)for(int j=1;j<=m;++j)f[i][j]= j < v[i] ? f[i-1][j] :max(f[i-1][j],f[i-1][j-v[i]]+w[i]);
+    printf("%d\n",f[n][m]);
+    return 0;
+}
+
+
+
+
+#include<iostream>
+using namespace std;
+const int N = 1e3+10;
 int n,m,v[N],w[N],f[N];
 int main(){
     cin>>n>>m;
