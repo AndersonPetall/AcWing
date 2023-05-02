bf47d385eb6ad2a7b6642f381668a99435e80f1f
AndersonPetall <andersonpetall@gmail.com>
2023-05-02 18:18:17 -0400

    Update 5-1.2.1 背包问题_3. 完全背包问题.cpp

--- 算法基础课/W5动态规划/5-1.2.1 背包问题_3. 完全背包问题.cpp
+++ 算法基础课/W5动态规划/5-1.2.1 背包问题_3. 完全背包问题.cpp
@@ -27,8 +27,8 @@ int main(){
     for(int i=1;i<=n;++i)cin>>v[i]>>w[i];
     for(int i=1; i<=n; ++i){
         for(int j=1; j<=m; ++j){
-            f[i][j] = f[i-1][j];  //赋初值
-            if(j-v[i]>=0)f[i][j]=max(f[i][j],f[i][j-v[i]]+w[i]);
+            f[i][j] = f[i-1][j];  //赋初值  必要
+            if(j-v[i]>=0)f[i][j]=max(f[i][j],f[i][j-v[i]]+w[i]);  //与01背包不同, 物品重复使用,所以不用i-1而是用i
         }
     }
     printf("%d\n",f[n][m]);
@@ -37,18 +37,21 @@ int main(){
 
 
 
-//一维优化版本    
+
+
 #include<iostream>
 using namespace std;
 const int N = 1e3 + 10;
-int v[N],w[N],f[N][N];
+int v[N],w[N],f[N];
 int main(){
     int n,m; cin>>n>>m;
     for(int i=1;i<=n;++i)cin>>v[i]>>w[i];
-    for(int i=1; i<=n; ++i){  //正序  和01背包不一样
-        for(int j=v[i]; j<=m; ++j)f[j]=max(f[j],f[j-v[i]]+w[i]);  //物品i的选取由此轮(i)的f值进行计算,所以使用正序
+    for(int i=1; i<=n; ++i){
+        for(int j=v[i]; j<=m; ++j){  //正序  和01背包不一样
+            f[j] = max(f[j], f[j-v[i]]+w[i]);  //物品i的选取由此轮(i)的f值进行计算,所以使用正序
+        }
     }
-    printf("%d\n",f[n][m]);
+    printf("%d\n",f[m]);
     return 0;
 }
 
@@ -60,11 +63,11 @@ using namespace std;
 const int N = 1e3 + 10;
 int f[N];
 int main(){
-    int n,m; cin>>n>>m;
-    for(int i=1; i<=n; ++i){\
-        int v,w; cin>>v>>w;
-        for(int j=v; j<=m; ++j)f[j]=max(f[j],f[j-v]+w);
+    int n, m; scanf("%d%d", &n, &m);
+    for(int i=1; i<=n; ++i){
+        int v, w; scanf("%d%d", &v, &w);
+        for(int j=v; j<=m; ++j) f[j] = max(f[j], f[j-v]+w);
     }
-    printf("%d\n",f[m]);
+    printf("%d\n", f[m]);
     return 0;
 }
