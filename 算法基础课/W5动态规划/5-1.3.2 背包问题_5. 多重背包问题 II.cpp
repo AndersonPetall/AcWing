7ec2c887e653c1110556ed9f46e5f325bf8832c3
AndersonPetall <andersonpetall@gmail.com>
2023-05-03 21:58:44 -0400

    Update and rename 5-1.1.0 背包问题_5. 多重背包问题 II.cpp to 5-1.3.2 背包问题_5. 多重背包问题 II.cpp

---
+++ 算法基础课/W5动态规划/5-1.3.2 背包问题_5. 多重背包问题 II.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+const int N = 12010, M = 2010;
+int v[N], w[N], f[M];
+int main(){
+    int n,m, cnt=0; scanf("%d%d", &n, &m);
+    for(int i=1; i<=n; ++i){
+        int a, b, s, k=1; scanf("%d%d%d", &a, &b, &s);
+        while(k<=s){
+            v[++cnt] = a*k;
+            w[cnt] = b*k;
+            s-=k;
+            k*=2;
+        }
+        if(s>0){
+            v[++cnt] = a*s;
+            w[cnt] = b*s;
+        }
+    }
+    n = cnt;
+    for(int i=1; i<=n; ++i) for(int j=m; j>=v[i]; --j) f[j] = max(f[j], f[j-v[i]]+w[i]);
+    printf("%d\n", f[m]);
+    return 0;
+}
