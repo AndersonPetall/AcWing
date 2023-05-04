0edb19a09cc3b01af26a178859de87238addc03f
AndersonPetall <andersonpetall@gmail.com>
2023-05-03 23:26:31 -0400

    Update 5-1.4.1 背包问题_9. 分组背包问题.cpp

--- 算法基础课/W5动态规划/5-1.4.1 背包问题_9. 分组背包问题.cpp
+++ 算法基础课/W5动态规划/5-1.4.1 背包问题_9. 分组背包问题.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 const int N = 110;
+int v[N][N], w[N][N], f[N][N], s[N];
+int main() {
+    int n, m; scanf ("%d%d", &n, &m);
+    for (int i=1; i<=n; ++i) {
+        scanf ("%d", &s[i]);
+        for (int j=1; j<=s[i]; ++j) scanf ("%d%d", &v[i][j], &w[i][j]);
+    }
+    for(int i=1; i<=n; ++i) for(int j=1; j<=m; ++j) {
+        f[i][j] = f[i-1][j];  //赋初值, 不选择的情况
+        for(int k=1; k<=s[i]; ++k) if(v[i][k]<=j) f[i][j] = max(f[i][j], f[i-1][j-v[i][k]] + w[i][k]);
+    }    
+    printf("%d\n", f[n][m]);
+    return 0;
+}
+
+
+
+
+#include <iostream>
+using namespace std;
+const int N = 110;
 int v[N][N], w[N][N], f[N], s[N];
 int main() {
     int n, m; scanf ("%d%d", &n, &m);
@@ -8,7 +29,7 @@ int main() {
         scanf ("%d", &s[i]);
         for (int j=1; j<=s[i]; ++j) scanf ("%d%d", &v[i][j], &w[i][j]);
     }
-    for(int i=1; i<=n; ++i) for(int j=m; j>=1; --j) for(int k=1; k<=s[i]; ++k) 
+    for(int i=1; i<=n; ++i) for(int j=m; j>=1; --j) for(int k=1; k<=s[i]; ++k)    //同01背包问题类似,逆序
         if(v[i][k]<=j) f[j] = max(f[j], f[j-v[i][k]] + w[i][k]);
     printf("%d\n", f[m]);
     return 0;
