756e36848842957ad7cb9034e41d8e0404fedc52
AndersonPetall <andersonpetall@gmail.com>
2024-11-02 18:08:58 -0400

    Update 1-1.1.1 排序_785. 快速排序.cpp

--- 算法基础课/W1基础语法/1-1.1.1 排序_785. 快速排序.cpp
+++ 算法基础课/W1基础语法/1-1.1.1 排序_785. 快速排序.cpp
@@ -1,16 +1,41 @@
+//j为分割版
+#include<iostream>
+using namespace std;
+const int N = 1e6 + 10;
+int n, q[N];
+void quick_sort(int q[], int l, int r){
+    if(l>=r) return; //递归结束标志
+    int i = l-1, j =r+1, x = q[(l+r)>>1];//边界问题
+    while(i < j){
+        do ++i; while(q[i] < x);
+        do --j; while(x < q[j]);
+        if(i<j) swap(q[i], q[j]);
+    }
+    quick_sort(q,l,j);quick_sort(q,j+1,r);   //边界问题
+}
+int main(){
+    scanf("%d", &n);
+    for(int i = 0; i < n; ++i) scanf("%d", &q[i]);
+    quick_sort(q,0,n-1);
+    for(int i = 0; i < n; ++i) printf("%d ", q[i]);
+    return 0;
+}
+
+
+//i为分割版
 #include<iostream>
 using namespace std;
 const int N = 1e6 + 10;
 int n, q[N];
 void quick_sort(int q[], int l, int r){
     if(l>=r) return;
-    int i = l-1, j =r+1, x = q[(l+r)>>1];
+    int i = l-1, j =r+1, x = q[(l+r+1)>>1];//边界问题
     while(i < j){
         do ++i; while(q[i] < x);
         do --j; while(x < q[j]);
         if(i<j) swap(q[i], q[j]);
     }
-    quick_sort(q,l,j);quick_sort(q,j+1,r);
+    quick_sort(q,l,i-1);quick_sort(q,i,r);//边界问题
 }
 int main(){
     scanf("%d", &n);
