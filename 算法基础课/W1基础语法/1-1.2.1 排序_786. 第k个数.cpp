f9db721abc1bda6b6da7d9f1f3c45184c4d63111
AndersonPetall <andersonpetall@gmail.com>
2024-11-04 20:08:15 -0500

    Update 1-1.2.1 排序_786. 第k个数.cpp

--- 算法基础课/W1基础语法/1-1.2.1 排序_786. 第k个数.cpp
+++ 算法基础课/W1基础语法/1-1.2.1 排序_786. 第k个数.cpp
@@ -7,7 +7,7 @@ int quick_sort(int q[], int l, int r, int k){
     if(l>=r) return q[l]; //递归结束标志
     int i=l-1,j=r+1,x=q[l+r>>1];//边界问题
     while(i<j){
-        do ++i;while(q[i]<x);do --j;while(x<q[j]);
+        while(q[++i] < x) ; while(x<q[--j]);
         if(i<j) swap(q[i],q[j]);
     }
     if(k<=j+1-l) return quick_sort(q,l,j,k); else return quick_sort(q,j+1,r,k-(j+1-l));//边界问题
@@ -29,7 +29,7 @@ int quick_sort(int q[], int l, int r, int k){
     if(l>=r) return q[l]; //递归结束标志
     int i=l-1,j=r+1,x=q[l+r+1>>1];//边界问题
     while(i<j){
-        do ++i;while(q[i]<x);do --j;while(x<q[j]);
+        while(q[++i] < x) ; while(x<q[--j]);
         if(i<j) swap(q[i],q[j]);
     }
     if(k<=i-l) return quick_sort(q,l,i-1,k); else return quick_sort(q,i,r,k-(i-l));//边界问题
