92f2845c47072fdd42f4ef7eb5a523fa052faa4c
AndersonPetall <andersonpetall@gmail.com>
2024-11-09 18:56:49 -0500

    Update 1-2.1.1 二分_789. 数的范围.cpp

--- 算法基础课/W1基础语法/1-2.1.1 二分_789. 数的范围.cpp
+++ 算法基础课/W1基础语法/1-2.1.1 二分_789. 数的范围.cpp
@@ -1,35 +1,58 @@
 #include<iostream>
 using namespace std;
-
-const int N = 1e6 + 10;
-int n, m;
-int q[N];
-
+int const N = 1e6+10;
+int n,m,q[N];
+int sl(int q[], int l, int r, int x){
+    while(l<r){
+        int mid=l+r>>1;
+        q[mid]>=x? r=mid : l=mid+1;
+    }
+    return q[l]==x? l:-1;
+}
+int sr(int q[], int l, int r, int x){
+    while(l<r){
+        int mid=l+r+1>>1;
+        x>=q[mid]? l=mid:r=mid-1;
+    }
+    return q[r]==x? r:-1;
+}
 int main(){
     scanf("%d%d", &n, &m);
-    for(int i = 0; i < n; ++i)scanf("%d", &q[i]);
-    
+    for(int i=0;i<n;++i)scanf("%d", &q[i]);
     while(m--){
-        int x;
-        scanf("%d", &x);
-        
-        int l = 0, r = n - 1;
-        while(l < r){
-            int mid = (l + r) >>1;
-            if(x <= q[mid]) r = mid;
-            else l = mid + 1;
-        }
-        if(q[l] != x) cout << "-1 -1" <<endl;
-        else{
-            cout << l << " ";
-            l = 0, r = n - 1;
-            while(l < r){
-                int mid = (l + r + 1) >>1;
-                if(q[mid] <= x) l = mid;
-                else r = mid - 1;
-            }
-            cout << l << endl;
-        }
+        int x;scanf("%d", &x);;
+        int l=0,r=n-1;
+        printf("%d %d\n", sl(q,0,n-1,x),sr(q,0,n-1,x));
+    }
+    return 0;
+}
+
+
+
+
+#include<iostream>
+using namespace std;
+int const N = 1e6+10;
+int n,m,q[N];
+int sl(int q[], int l, int r, int x){
+    while(l+1<r){//左边<， 右边>=
+        int mid=l+r>>1; q[mid] < x ? l=mid: r=mid;
+    }
+    return q[r]==x? r:-1;
+}
+int sr(int q[], int l, int r, int x){
+    while(l+1<r){//左边<=， 右边>
+        int mid=l+r>>1; q[mid] > x ? r=mid : l = mid;
+    }
+    return q[l]==x? l:-1;
+}
+int main(){
+    scanf("%d%d", &n, &m);
+    for(int i=0;i<n;++i)scanf("%d", &q[i]);
+    while(m--){
+        int x;scanf("%d", &x);;
+        int l=0,r=n-1;
+        printf("%d %d\n", sl(q,-1,n,x),sr(q,-1,n,x));
     }
     return 0;
 }
