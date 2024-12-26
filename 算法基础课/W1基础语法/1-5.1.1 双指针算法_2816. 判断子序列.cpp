9cf28951631bba87540b6e43fab005858878d7a4
AndersonPetall <andersonpetall@gmail.com>
2024-12-26 16:17:52 -0500

    Update 1-5.1.1 双指针算法_2816. 判断子序列.cpp

--- 算法基础课/W1基础语法/1-5.1.1 双指针算法_2816. 判断子序列.cpp
+++ 算法基础课/W1基础语法/1-5.1.1 双指针算法_2816. 判断子序列.cpp
@@ -14,3 +14,19 @@ int main(){
     if (j != n) {puts("No");}
     return 0;
 }
+
+
+
+#include<iostream>
+using namespace std;
+const int N=1e5+10;
+int a[N],b[N],n,m;
+int main(){
+    scanf("%d%d", &n,&m);
+    for(int i=0;i<n;++i)scanf("%d",&a[i]);
+    for(int j=0;j<m;++j)scanf("%d",&b[j]);
+    int i=0;
+    for(int j=0;j<m;++j) if(i<n&&a[i]==b[j]) i++;
+    i == n? puts("Yes"):puts("No");
+    return 0;
+}
