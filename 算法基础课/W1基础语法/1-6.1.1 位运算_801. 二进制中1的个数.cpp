46d7140cb15458e45143638821b21f1b0c40b489
AndersonPetall <andersonpetall@gmail.com>
2024-12-26 17:04:50 -0500

    Update 1-6.1.1 位运算_801. 二进制中1的个数.cpp

--- 算法基础课/W1基础语法/1-6.1.1 位运算_801. 二进制中1的个数.cpp
+++ 算法基础课/W1基础语法/1-6.1.1 位运算_801. 二进制中1的个数.cpp
@@ -1,37 +1,27 @@
 #include<iostream>
 using namespace std;
-int n;
 int lowbit(int x){
-    return x & (~x + 1);
+    return x&(-x); // return x & (~x + 1);
 }
 int main(){
-    cin>> n;
-    int m;
+    int n;scanf("%d",&n);
     while(n--){
-        cin >> m;
-        int temp = 0;
-        while(m){
-            temp ++;
-            m -= lowbit(m);
-        }
-        printf("%d ", temp);
+        int x,res=0;scanf("%d",&x);
+        while(x) x-=lowbit(x),res++;
+        printf("%d ",res);
     }
     return 0;
 }
 
 
-
-
 #include<iostream>
 using namespace std;
-int n, m;
 int main(){
-    scanf("%d", &n);
+    int n;scanf("%d",&n);
     while(n--){
-        scanf("%d", &m);
-        int temp = 0;
-        for(int i = m; m; m -= m & -m) temp++;
-        printf("%d ", temp);
+        int x,res=0;scanf("%d",&x);
+        for(int i=x;x;x-=x&-x)res++;
+        printf("%d ",res);
     }
     return 0;
 }
